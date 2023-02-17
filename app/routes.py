384da9f6cfca9ab461e604e2adefcb29d304efdc
roman345543 <125037843+roman345543@users.noreply.github.com>
2023-02-17 16:18:48 +0300

    made some changes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import render_templates
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = { "username":'Roman' }
+    return render_templates('index.html',title='Home',user=user)
