5414183f1d6cb378ace48f0e8e936dba9f581510
pavst23 <p.stupnikov0723@mail.ru>
2023-04-07 17:20:56 +0300

    made some changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLALchemy
+from flask_nigrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLALchemy(app)
+nigrate = Migrate
+
 
 from app import routes
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or '34554367#_roman'
-    
+    SQLALCHENY_DATABASE_URL = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir,"app.db")
+    SQLALCHENY_TRACK_MODIFICATIONS = False
