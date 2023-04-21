4fe429acb400227c77682fa86974e859d9e9a9c5
pavst23 <p.stupnikov0723@mail.ru>
2023-04-21 17:08:32 +0300

    made some changes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<user {self.username}>"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(1500))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
 from app.forms import LoginFrom
-from flask import flash, redirect
+from flask import flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -21,8 +21,11 @@ def index():
             'body': 'товарищ андрей'
         }
     ]
-    return render_template('index.html',user=user,posts=posts)
+    return render_template('index.html',user=user, title="Home",posts=posts)
 @app.route("/login",methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit()
+        flash(f"{form.username.date} sign in successfully")
+        return redirect(url_for("index"))
     return render_template("login.html", title="login", form=form)
